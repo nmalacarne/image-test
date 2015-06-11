e25e518bc398891d0985dbb2861edf135876fc29
Nicholas Malacarne <nick@craterdust.com>
2015-06-11 15:13:48 -0400

    renamed variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var path  = require('path');
 var fs    = require('fs');
-var im    = require('gm').subClass({imageMagick: true});
+var gm    = require('gm').subClass({imageMagick: true});
 
 var readPath  = path.join(__dirname, 'old');
 var writePath = path.join(__dirname, 'new');
@@ -8,7 +8,7 @@ var writePath = path.join(__dirname, 'new');
 fs.readdir(readPath, function(err, files) {
   if (!err) {
     files.forEach(function(file) {
-      im(path.join(readPath, file))
+      gm(path.join(readPath, file))
       .resize(400, 300)
       .write(path.join(writePath, file), function(err) {
         if (!err) console.log(file + ' resized.');
