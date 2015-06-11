7ff82cb4307b4df81a62c55367d4f033eb228750
Nicholas Malacarne <nick@craterdust.com>
2015-06-11 15:20:07 -0400

    small changes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var path  = require('path');
 var fs    = require('fs');
 var gm    = require('gm').subClass({imageMagick: true});
 
+var width   = 400;
+var height  = 300;
+
 var readPath  = path.join(__dirname, 'old');
 var writePath = path.join(__dirname, 'new');
 
@@ -9,9 +12,9 @@ fs.readdir(readPath, function(err, files) {
   if (!err) {
     files.forEach(function(file) {
       gm(path.join(readPath, file))
-      .resize(400, 300)
+      .resize(width, height)
       .write(path.join(writePath, file), function(err) {
-        if (!err) console.log(file + ' resized.');
+        if (!err) console.log('%s resized.', file);
       });
     });
   }
